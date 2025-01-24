715c933cae0530c51d4ac80cd0b82105e3ac03b8
Christian Burroughs <christianjburroughs@me.com>
2025-01-24 08:55:50 -0600

    configuretailwind

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,5 @@
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../.env') });
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI);
